e861ef992fe64fda848b0a55c46a4445c9f5689d
xrj777 <100856625+xrj777@users.noreply.github.com>
2022-03-03 23:23:19 +0800

    Add files via upload

---
+++ XuRunjing0221App/W1HW2App/DayPrinter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW2App
+{
+    internal class DayPrinter
+    {
+        static void Main(string[] args)
+        {
+            String[] days = { "Mondey", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+        }
+    }
+}
