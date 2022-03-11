d9b06e8a990066ad219e09106c11000ed370ae3e
xrj777 <100856625+xrj777@users.noreply.github.com>
2022-03-11 13:45:24 +0800

    Add files via upload

---
+++ XuRunjing0221ERSWindowsFormsApp/XuRunjing0221ERSWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace XuRunjing0221ERSWindowsFormsApp
+{
+    partial class EmployeeRecordsFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.statusBarPanel1 = new System.Windows.Forms.StatusBarPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.statusBarPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 7);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(240, 352);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(246, 7);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(438, 352);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 359);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Panels.AddRange(new System.Windows.Forms.StatusBarPanel[] {
+            this.statusBarPanel1});
+            this.statusBar1.ShowPanels = true;
+            this.statusBar1.Size = new System.Drawing.Size(731, 22);
+            this.statusBar1.TabIndex = 2;
+            this.statusBar1.Text = "statusBar1";
+            // 
+            // statusBarPanel1
+            // 
+            this.statusBarPanel1.Name = "statusBarPanel1";
+            this.statusBarPanel1.Text = "Click the employee code to view details";
+            this.statusBarPanel1.Width = 240;
+            // 
+            // EmployeeRecordsFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(752, 361);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EmployeeRecordsFrom";
+            this.Text = "Empolyee Records Monitoring System";
+            ((System.ComponentModel.ISupportInitialize)(this.statusBarPanel1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void listView1_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusBar statusBar1;
+        private System.Windows.Forms.StatusBarPanel statusBarPanel1;
+    }
+}
+
---
+++ XuRunjing0221ERSWindowsFormsApp/XuRunjing0221ERSWindowsFormsApp/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XuRunjing0221ERSWindowsFormsApp
+{
+    public partial class EmployeeRecordsFrom : Form
+    {
+        private TreeNode tvRootNode;
+
+        public EmployeeRecordsFrom()
+        {
+            InitializeComponent();
+            PopulateTreeView();
+        }
+
+       
+
+        private void EmpolyeeRecordsForm_Load(object sender, EventArgs e)
+        {
+         
+        }
+        private void PopulateTreeView()
+        {
+            statusBarPanel1.Tag = "Refeshing Empolyee Code. Please Wait...";
+            this.Cursor = Cursors.WaitCursor;
+            treeView1.Nodes.Clear();
+            tvRootNode = new TreeNode("Empolyee Records");
+            this.Cursor=Cursors.Default;
+            treeView1.Nodes.Add(tvRootNode);
+
+            TreeNodeCollection nodeCollection = tvRootNode.Nodes;
+            XmlTextReader reader = new XmlTextReader("E:\\系统默认\\桌面\\CASE_2021-22\\MyRepos\\XuRunjing0221ERSWindowsFormsApp\\XuRunjing0221ERSWindowsFormsApp\\EmpRec.xml");
+            reader.MoveToElement();
+            try
+            {
+                while (reader.Read())
+                {
+                    if(reader.HasAttributes && reader.NodeType == XmlNodeType.Element)
+                    {
+                        reader.MoveToElement();//<EmpRecordsData>
+                        reader.MoveToElement();//<Ecode
+
+                        reader.MoveToAttribute("Id");//Id="E001"
+                        String strVal = reader.Value;
+
+                        reader.Read();
+                        reader.Read();
+                        if (reader.Name == "Dept")
+                        {
+                            reader.Read();
+                        }
+                        //create a child node
+                        TreeNode EcodeNode = new TreeNode(strVal);
+                        //add the node
+                        nodeCollection.Add(EcodeNode);
+                    }
+                }
+                statusBarPanel1.Tag = "Click on an empolyee code to see their record.";
+            }
+            catch(XmlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }//end
+        protected void InitalizeListControl()
+        {
+            listView1.Clear();
+            listView1.Columns.Add("Empolyee Name",255,HorizontalAlignment.Left);
+            listView1.Columns.Add("Date of Join",70,HorizontalAlignment.Right);
+            listView1.Columns.Add("Gread",105,HorizontalAlignment.Left);
+            listView1.Columns.Add("Salary",105,HorizontalAlignment.Left);
+        }
+        protected void PopulateListView(TreeNode crrNode)
+        {
+            InitalizeListControl();
+            XmlTextReader listRead = new XmlTextReader("E:\\系统默认\\桌面\\CASE_2021-22\\MyRepos\\XuRunjing0221ERSWindowsFormsApp\\XuRunjing0221ERSWindowsFormsApp\\EmpRec.xml");
+            listRead.MoveToElement();
+            while (listRead.Read())
+            {
+                string strNodeName;
+                string strNodePath;
+                string name;
+                string gread;
+                string doj;
+                string sal;
+                string[] strItmsArr=new string[4];
+                listRead.MoveToFirstAttribute();
+                strNodeName = listRead.Value;
+                strNodePath = crrNode.FullPath.Remove(0, 17);
+                if (strNodePath == strNodeName)
+                {
+                    ListViewItem lvi;
+
+                    listRead.MoveToNextAttribute();
+                    name = listRead.Value;
+                    lvi = listView1.Items.Add(listRead.Value);
+
+                    listRead.Read();
+                    listRead.Read();
+
+                    listRead.MoveToFirstAttribute();
+                    doj= listRead.Value;
+                    lvi.SubItems.Add(doj);
+
+                    listRead.MoveToNextAttribute();
+                    gread= listRead.Value;
+                    lvi.SubItems.Add(gread);
+
+                    listRead.MoveToNextAttribute();
+                    sal= listRead.Value;
+                    lvi.SubItems.Add(sal);
+
+                    listRead.MoveToNextAttribute();
+                    listRead.MoveToElement();
+                    listRead.ReadString();
+                }
+            }
+        }//end
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode currNode=e.Node;
+            if (tvRootNode == currNode)
+            {
+                InitalizeListControl();
+                statusBarPanel1.Text = "Double Click the Empolyee Records";
+                return;
+            }
+            else
+            {
+                statusBarPanel1.Text = "Click an Empolyee code to view individual record";
+            }
+            PopulateListView(currNode);
+        }
+    }
+}
